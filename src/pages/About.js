1a608de867716bc9c1fb631e6d280f2b411a922c
Raamprasaadh <ramprasadhsurya@gmail.com>
2020-06-22 19:18:07 +0530

    Changes content and style of about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,21 +5,43 @@ class About extends Component {
     render() {
         return (
             <div className="aboutUs">
-            <h2>What we do</h2><br></br>
-            <p>We at "Abirami Areca Plates" aim to reduce the impact on our environment by creating and promoting Areca Leaf products which are being produced with sustainable resource(made from fallen leaves - 100 % natural) through Eco friendly process(no chemicals or additives used) and make sure it is acceptable to Earth when disposed(biodegradable & compostable).</p >
-            <br></br>
-            <br></br>
-            
-            <h2>VISION</h2><br></br>
-            <p>To be a world class, leading eco-friendly product manufacturer who makes our customers, employees, suppliers, shareholders, society and environment smile.</p>
-            <br></br>
-            <br></br>
 
-            <h2>MISSION</h2><br></br>
-            <p>We are committed to be a highly creative and socially responsible eco-friendly product manufacturer, by delighting our customers with our responsive attitude, by introducing innovative products with superior quality & reliability at an affordable cost and by enhancing the organization's abilities to compete in the global market with minimal environmental impact.</p>
-            <br></br>
-            <br></br>
-        </div >
+<h2>What we do</h2><br></br>
+<h3>Using Bio-Degradable is New responsible.</h3>
+<br></br>
+<p>Yes, We have to be responsible in order to give the present world to our children and next generation in a better form.</p>
+<p>In short we (Abirami Areca Products) are being responsible!</p>
+
+
+<h2>How it all started</h2><br></br>
+<p>Basically we are farm owners. to take care of our farm and environment we started to avoid plastics then we realized plastics have become a part of our life without realization.</p>
+<p>one area where we mainly struggled is providing food for our employees.</p>
+<p>Then we slowly started to reduce the use of plastics and replaced them with Areca products.</p>
+
+<h2>VISION</h2><br></br>
+<p>Then we thought we should help others to be responsible also. After all we are here to look out for each other.</p>
+<br></br>
+<p>Our vision is to produce more Eco friendly products and slowly replace plastics in the field of food through out the world.</p>
+
+<h2>Perks of using Areca Products. </h2><br></br>
+
+<p>Below are some perks of using Areca products.</p><br></br>
+
+<p>1)First and fore most it is Eco-friendly So we do not have to worry about "How to dispose this without Harming Nature ?".</p>
+<p>2)Unlike plastics when in contact with hot food items Areca products produces some medicinal facts.</p>
+<p>3)Last but not least by using our products. you are providing food for two rural families indirectly.</p>
+<br></br>
+<br></br>
+<h2>Manufacturing Process. </h2><br></br>
+<p>When it comes to hygine and cleanliness we are a bit of a freak. We make sure our product quality is 100%.
+By strictly following deciplined Manufacturing process.</p>
+<br></br>
+<br></br>
+
+<b>We strongly believe small changes make big difference and started doing our part for the environment.</b>
+
+<b>How about you, are you responsible ?</b>
+</div >
     )
     }
 }
