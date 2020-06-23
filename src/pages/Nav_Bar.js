cd2cbba9f20578929faba537a579968b9fc5d0d1
Raamprasaadh <ramprasadhsurya@gmail.com>
2020-06-23 15:55:16 +0530

    Adds Dummy logo font.

--- src/pages/Nav_Bar.js
+++ src/pages/Nav_Bar.js
@@ -2,21 +2,38 @@
 //ToDo add Logo
 //check why it is not loading pages without location.reload
 import React, { Component } from 'react';
-import {HashRouter, NavLink} from 'react-router-dom';
-
+import { HashRouter, NavLink } from 'react-router-dom';
+import Logo from '../../src/logo.svg'
 import '../css/style.css';
 class NavBar extends Component {
-    render(){
-        return(
-            <div className="navBar">
-                <HashRouter>
-                <ul>
-                    <li onClick={()=>{window.location.reload(false);}}><NavLink exact to={process.env.PUBLIC_URL +"/"}>Home</NavLink></li>
-                    <li onClick={()=>{window.location.reload(false);}}><NavLink to={process.env.PUBLIC_URL +"/products"}>Products</NavLink></li>
-                    <li onClick={()=>{window.location.reload(false);}}><NavLink to={process.env.PUBLIC_URL +"/about"}>About Us</NavLink></li>
-                    <li onClick={()=>{window.location.reload(false);}}><NavLink to={process.env.PUBLIC_URL +"/contact"}>Contact Us</NavLink></li>
-                </ul>
-                </HashRouter>
+    render() {
+        return (
+            <div>
+                <div className="navBar">
+
+                    <HashRouter>
+                        {/*<img src={Logo} className="logo"  />*/}
+                       {/* <ul className = "navLeft">
+                            <li onClick={() => { window.location.reload(false); }}><NavLink to={process.env.PUBLIC_URL + "/"}><b>AAP</b></NavLink></li>
+        </ul>*/}
+                        <ul className = "navRight">
+                            <li onClick={() => { window.location.reload(false); }}><NavLink to={process.env.PUBLIC_URL + "/"}><b>AAP</b></NavLink></li>
+                            <li onClick={() => { window.location.reload(false); }}><NavLink exact to={process.env.PUBLIC_URL + "/home"}>Home</NavLink></li>
+                            <li onClick={() => { window.location.reload(false); }}><NavLink to={process.env.PUBLIC_URL + "/products"}>Products</NavLink></li>
+                            <li onClick={() => { window.location.reload(false); }}><NavLink to={process.env.PUBLIC_URL + "/about"}>About Us</NavLink></li>
+                            <li onClick={() => { window.location.reload(false); }}><NavLink to={process.env.PUBLIC_URL + "/contact"}>Contact Us</NavLink></li>
+                        </ul>
+                    </HashRouter>
+
+                </div>
+                <div className="navBar navRight">
+                    <HashRouter>
+                        <ul>
+
+                        </ul>
+                    </HashRouter>
+
+                </div>
             </div>
         );
     }
